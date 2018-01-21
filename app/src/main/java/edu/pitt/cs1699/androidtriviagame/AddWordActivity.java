3ab24a738a41e47f509f782ab015d81a44a17032
Jordan Grogan <jordangrogan11@gmail.com>
2018-01-21 11:02:10 -0500

    Add Word Activity design

---
+++ app/src/main/java/edu/pitt/cs1699/androidtriviagame/AddWordActivity.java
@@ -0,0 +1,13 @@
+package edu.pitt.cs1699.androidtriviagame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_word);
+    }
+}
