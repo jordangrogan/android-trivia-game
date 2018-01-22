e8447d721972b87b6baebdfc48d7de068b6888be
Jordan Grogan <jordangrogan11@gmail.com>
2018-01-21 21:21:12 -0500

    Activity navigation

--- app/src/main/java/edu/pitt/cs1699/androidtriviagame/ScoreHistoryActivity.java
+++ app/src/main/java/edu/pitt/cs1699/androidtriviagame/ScoreHistoryActivity.java
@@ -1,7 +1,9 @@
 package edu.pitt.cs1699.androidtriviagame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ScoreHistoryActivity extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class ScoreHistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_history);
     }
+
+    public void back(View view) {
+        finish(); // Will finish this Activity & go back to the Start Activity
+    }
+
 }
--- app/src/main/java/edu/pitt/cs1699/androidtriviagame/StartActivity.java
+++ app/src/main/java/edu/pitt/cs1699/androidtriviagame/StartActivity.java
@@ -2,6 +2,8 @@ package edu.pitt.cs1699.androidtriviagame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,4 +12,20 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
     }
+
+    public void play(View view) {
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
+    }
+
+    public void addWord(View view) {
+        Intent intent = new Intent(this, AddWordActivity.class);
+        startActivity(intent);
+    }
+
+    public void scoreHistory(View view) {
+        Intent intent = new Intent(this, ScoreHistoryActivity.class);
+        startActivity(intent);
+    }
+
 }
