69bdc7699145f598941d5ac3d5ac7fe6ae772467
Jordan Grogan <jordangrogan11@gmail.com>
2018-01-24 19:04:36 -0500

    Add word to custom file and create word list in Play Activity

--- app/src/main/java/edu/pitt/cs1699/androidtriviagame/AddWordActivity.java
+++ app/src/main/java/edu/pitt/cs1699/androidtriviagame/AddWordActivity.java
@@ -2,6 +2,15 @@ package edu.pitt.cs1699.androidtriviagame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 public class AddWordActivity extends AppCompatActivity {
 
@@ -9,5 +18,31 @@ public class AddWordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_word);
+    }
+
+    public void addWord(View view) {
+
+        EditText txtWord = findViewById(R.id.txtWord);
+        String word = txtWord.getText().toString();
+
+        EditText txtDef = findViewById(R.id.txtDef);
+        String def = txtDef.getText().toString();
+
+        PrintStream output = null;
+        try {
+            output = new PrintStream(
+                    openFileOutput("customwordsdefinitions.txt", MODE_APPEND));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        output.println(word);
+        output.println(def);
+        output.close();
+
+        Log.d("Add Word", "File written to");
+
+        Toast.makeText(this, "Word Added", Toast.LENGTH_SHORT).show();
+        finish(); // Will finish this Activity & go back to the Start Activity
+
     }
 }
--- app/src/main/java/edu/pitt/cs1699/androidtriviagame/PlayActivity.java
+++ app/src/main/java/edu/pitt/cs1699/androidtriviagame/PlayActivity.java
@@ -2,12 +2,58 @@ package edu.pitt.cs1699.androidtriviagame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PlayActivity extends AppCompatActivity {
+
+    private static ArrayList<Word> words = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+
+        // ADD WORDS FROM RESOURCE
+        Scanner scan = new Scanner(
+                getResources().openRawResource(R.raw.wordsdefinitions));
+        while (scan.hasNextLine()) {
+            String w = scan.nextLine();
+            Log.d("Word:", w);
+            String d = scan.nextLine();
+            Log.d("Definition:", d);
+            words.add(new Word(w,d));
+        }
+
+        // ADD WORDS FROM CUSTOM WORDS DEFINITIONS FILE
+        try {
+            scan = new Scanner(
+                    openFileInput("customwordsdefinitions.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (scan.hasNextLine()) {
+            String w = scan.nextLine();
+            Log.d("Word:", w);
+            String d = scan.nextLine();
+            Log.d("Definition:", d);
+            words.add(new Word(w,d));
+        }
+
+        scan.close();
+
+    }
+
+    private class Word {
+        public String word;
+        public String def;
+        public Word(String w, String d) {
+            word = w;
+            def = d;
+        }
     }
 }
